012ee3dc731a9dfb3e577b9c33532909f5e81818
Ankit Kumar <ankitkrai4n0522@gmail.com>
2023-08-10 23:58:52 +0530

    app

--- redux-config/src/App.js
+++ redux-config/src/App.js
@@ -1,8 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Todo } from "./components/Todo";
+import { Counter } from "./components/Counter";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Counter />
+      <Todo />
+    </div>
+  );
 }
 
 export default App;
